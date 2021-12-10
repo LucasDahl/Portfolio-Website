95c83e05c1301242a2243e2016fd8f1c0ea35020
Lucas Dahl <LucasDahl425@yahoo.com>
2021-12-09 22:23:22 -0800

    Added cards for projects.

---
+++ main.js
@@ -0,0 +1,8 @@
+const card = document.querySelectorAll(".card__inner");
+
+function flipCard() {
+  this.classList.toggle('is-flipped');
+}
+
+
+card.forEach((card) => card.addEventListener("click", flipCard));
